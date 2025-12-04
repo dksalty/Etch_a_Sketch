61033334a5a666d20f12b8ab80c4a11cfe912a07
dksalty <rollinmelson@gmail.com>
2025-12-03 19:50:14 -0500

    Added erasers, buttons, and centered.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const container = document.getElementById('container');
 const button = document.getElementById('button');
-
+const randomColor = document.getElementById('randomColors');
+const black = document.getElementById('black');
+const erase = document.getElementById('erase');
+const eraseAll = document.getElementById('eraseAll');
 let size = 16; 
 createGrid(size);
 
@@ -25,19 +28,47 @@ function getRandomRgbColor() {
  return `rgb(${r}, ${g}, ${b})`;
 }
 
+randomColor.addEventListener('click', () =>{
 
 container.addEventListener('mouseover', (e) => {
   if (e.target.classList.contains('children')) {
     e.target.style.backgroundColor =  getRandomRgbColor();
   }
 }, true);
+});
+
+black.addEventListener('click', () => {
+  container.addEventListener('mouseover', (e) => {
+  if (e.target.classList.contains('children')) {
+    e.target.style.backgroundColor = 'black';
+  }
+}, true);
+});
+
+erase.addEventListener('click', () => {
+  container.addEventListener('mouseover', (e) => {
+    if (e.target.classList.contains('children')) {
+      e.target.style.backgroundColor = 'white';
+    }
+  }, true);
+});
+
+eraseAll.addEventListener('click', () => {
+  const pixels = document.querySelectorAll('.children');
+  pixels.forEach(pixel => {
+    pixel.style.backgroundColor = 'white';
+  });
+});
+ 
+
+
 
 
 button.addEventListener('click', () => {
   const resize = Number(prompt('How many squares on each side?  (100 max)'));
 
   if (isNaN(resize) || resize <= 0) {
-    alert('Please enter a valid positive number');
+    alert('Please enter a valid number');
     return;
    }
    else if(resize > 100){
